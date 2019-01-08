135c73967479ff3af611e28d0ca88ef9f1a153ce
Fabrícia Diniz <afabriciadiniz@gmail.com>
2019-01-08 00:49:30 -0200

    correção banco de dados, nenhum problema

--- mysite/temas/models.py
+++ mysite/temas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Temas(models.Model):
+    
     tema_text = models.CharField(max_length=200)
     perguntas_tema = models.ForeignKey('polls.Perguntas', on_delete=models.DO_NOTHING, null=True, default=None)
     
