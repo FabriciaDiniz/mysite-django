e8beadef9da49e56e1a770ec2ff02df5e068bcdd
Fabrícia Diniz <afabriciadiniz@gmail.com>
2019-05-17 15:14:38 -0300

    ajustes pequenos perguntas

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', questoesView.as_view(), name='perguntas-index'),
     path('adiciona/', AdicionaPerguntaView.as_view(), name='perguntas-adiciona'),
     path('<pk>/', DetailView.as_view(), name='perguntas-mostra'),
+    path('<int:question_id>/results/', results, name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
     path('<pk>/edit/', edit, name='perguntas-edit'),
     path('<pk>/delete/', delete, name='perguntas-delete'),
 ]
